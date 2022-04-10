0b3140cc70dc2da2b9f1283bb9c2e63ac484e075
kevindigo <kevins@qualitycode.com>
2022-04-10 08:52:25 -0700

    Implement simple Play Creature

--- __tests__/doer.test.ts
+++ __tests__/doer.test.ts
@@ -1,7 +1,8 @@
 import { doDeed } from '../src/doer';
-import { Card, Deed, Player } from '../src/models';
+import { Card, Deed, Player, State } from '../src/models';
+import { SideManager } from '../src/side';
 import { createInitialState, StateManager } from '../src/state';
-import { DeedType } from '../src/types';
+import { CardNumber, CardState, DeedType } from '../src/types';
 
 const sig: Player = {
     name: 'Sig',
@@ -12,15 +13,26 @@ const marla: Player = {
     deckId: '679a6701-d7c3-494e-becb-04e9178aca30',
 };
 
+function createCard(cardNumber: CardNumber): Card {
+    return {
+        cardId: `OmegaCodex-${cardNumber}`,
+        deckId: 'Whatever',
+    };
+}
+
 describe('The deed doer', () => {
-    it('Can play an action with no effects', () => {
-        const state = createInitialState(sig, marla);
-        const stateManager = new StateManager(state);
-        const mySideManager = stateManager.getMySideManager();
-        const nothingToSee: Card = {
-            cardId: 'OmegaCodex-100',
-            deckId: 'Whatever',
-        };
+    let state: State;
+    let stateManager: StateManager;
+    let mySideManager: SideManager;
+
+    beforeEach(() => {
+        state = createInitialState(sig, marla);
+        stateManager = new StateManager(state);
+        mySideManager = stateManager.getMySideManager();
+    });
+
+    it('Can play an action with no Play effects', () => {
+        const nothingToSee = createCard('100');
         const hand = mySideManager.hand;
         hand.push(nothingToSee);
 
@@ -31,9 +43,44 @@ describe('The deed doer', () => {
             arsenalIndex: null,
         };
         doDeed(state, deed);
+
         expect(hand.length).toEqual(0);
+        expect(mySideManager.line.length).toEqual(0);
+        expect(mySideManager.arsenal.length).toEqual(0);
+        expect(mySideManager.discards.length).toEqual(0);
+        expect(mySideManager.scored.length).toEqual(1);
+        expect(mySideManager.drawPile.length).toEqual(17);
+
         const scored = mySideManager.scored;
-        expect(scored.length).toEqual(1);
         expect(scored[0]).toEqual(nothingToSee);
+
+        expect(state.turnState.turnFlags.canDiscard).toBeTruthy();
+    });
+
+    it('Can play a Creature with no Play effects to an empty line', () => {
+        const vix = createCard('001');
+        const hand = mySideManager.hand;
+        hand.push(vix);
+
+        const deed: Deed = {
+            type: DeedType.PLAY,
+            handIndex: 0,
+            lineIndex: 0,
+            arsenalIndex: null,
+        };
+        doDeed(state, deed);
+        expect(hand.length).toEqual(0);
+        expect(mySideManager.line.length).toEqual(1);
+        expect(mySideManager.arsenal.length).toEqual(0);
+        expect(mySideManager.discards.length).toEqual(0);
+        expect(mySideManager.scored.length).toEqual(0);
+        expect(mySideManager.drawPile.length).toEqual(17);
+
+        const line = mySideManager.line;
+        const cardWithState = line[0];
+        expect(cardWithState?.card).toEqual(vix);
+        expect(cardWithState?.state).toEqual(CardState.DORMANT);
+
+        expect(state.turnState.turnFlags.canDiscard).toBeTruthy();
     });
 });
--- src/doer.ts
+++ src/doer.ts
@@ -1,14 +1,55 @@
-import { Card, Deed, State } from './models';
+import { Card, CardWithState, Deed, State } from './models';
+import { CardefPool } from './pool';
 import { StateManager } from './state';
+import { CardState, CardType, DeedType } from './types';
 
-export function doDeed(state: State, deed: Deed): void {
+function doDeedPlay(state: State, deed: Deed): void {
     const stateManager = new StateManager(state);
     const mySideManager = stateManager.getMySideManager();
 
     const handIndex = deed.handIndex;
-    if (handIndex !== null) {
-        const card = mySideManager.hand[handIndex] as Card;
-        mySideManager.hand.splice(handIndex, 1);
-        mySideManager.scored.push(card);
+    if (handIndex === null) {
+        throw new Error(`Attempted to play with no handIndex`);
+    }
+
+    const card = mySideManager.hand[handIndex] as Card;
+    const pool = CardefPool.getPool();
+    const cardef = pool.lookup(card.cardId);
+    switch (cardef?.type) {
+        case CardType.ACTION: {
+            mySideManager.hand.splice(handIndex, 1);
+            mySideManager.scored.push(card);
+            break;
+        }
+        case CardType.CREATURE: {
+            const lineIndex = deed.lineIndex;
+            if (lineIndex === null) {
+                throw new Error('Attempted to play Creature with no lineIndex');
+            }
+            const cardWithState: CardWithState = {
+                card,
+                state: CardState.DORMANT,
+            };
+            mySideManager.hand.splice(handIndex, 1);
+            mySideManager.line.splice(lineIndex, 0, cardWithState);
+            break;
+        }
+        default:
+            throw new Error(
+                `Attempted to play card ${card.cardId} of unknown type ${cardef?.type}`
+            );
+    }
+
+    state.turnState.turnFlags.canDiscard = true;
+}
+
+export function doDeed(state: State, deed: Deed): void {
+    switch (deed.type) {
+        case DeedType.PLAY: {
+            doDeedPlay(state, deed);
+            break;
+        }
+        default:
+            throw new Error(`Can't do deed of type ${deed.type}`);
     }
 }
